bbce0c3f9ddaf86b38188fcc2ff37afe683b0cce
Alex <magefast@gmail.com>
2022-05-11 19:26:28 +0300

    commit

--- Console/Command/Qprocess.php
+++ Console/Command/Qprocess.php
@@ -2,12 +2,30 @@
 
 namespace Magefast\Qprocess\Console\Command;
 
+use Magefast\Qprocess\Service\Add;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Qprocess extends Command
 {
+    /**
+     * @var Add
+     */
+    private $addService;
+
+    /**
+     * @param Add $addService
+     * @param string|null $name
+     */
+    public function __construct(Add $addService, string $name = null)
+    {
+        $this->addService = $addService;
+
+        parent::__construct($name);
+
+    }
+
     /**
      * Initialization of the command.
      */
@@ -28,8 +46,8 @@ class Qprocess extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
-        // todo: implement CLI command logic here
-
-
+        $value = 'test' . uniqid();
+        $this->addService->execute($value);
+        $output->writeln('<info>' . 'Added value: ' . $value . '</info>');
     }
 }
